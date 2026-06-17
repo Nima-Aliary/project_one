7d97c4a0f7750e81cfd5a7d64be9e187c7f89170
nima <nimaaliary765@gmail.com>
2026-06-17 18:17:25 +0330

    finished code

--- git_1.py
+++ git_1.py
@@ -14,3 +14,30 @@ from sklearn.ensemble import RandomForestClassifier, GradientBoostingRegressor, 
 from xgboost import XGBClassifier, XGBRegressor
 from sklearn.metrics import accuracy_score, classification_report, precision_score, recall_score, f1_score, r2_score, mean_squared_error, mean_absolute_error
 
+bc = load_breast_cancer()
+X, y = bc.data, bc.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=42)
+
+pipe = Pipeline([
+    ('scale', StandardScaler()),
+    ('model', GradientBoostingClassifier(random_state=42))
+])
+
+param_grid = {
+    'model__n_estimators':[50, 100, 200],
+    "model__learning_rate": [0.01, 0.05, 0.1],
+    'model__max_depth':[None, 5, 10],
+    "model__subsample": [0.5, 0.8, 1.0],
+    'model__min_samples_leaf':[1, 2, 4]
+}
+
+cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
+
+grid = GridSearchCV(
+    estimator=pipe,
+    param_grid= param_grid,
+    cv = cv,
+    scoring='f1_macro',
+    n_jobs=-1,
+    verbose=1
+)
